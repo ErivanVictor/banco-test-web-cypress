c73d12307facfbcae81bb1d36a62bc2309515c59
Erivan <erivanvictorev2b@gmail.com>
2025-08-08 19:42:19 -0300

    adicionando comandos no fistures

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,17 +4,19 @@ describe('login', () => {
   })
   
   it('login com dados validos deve permitir o acesso', () => {
-
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
     cy.contains('button','Entrar').click()
-    cy.contains('h4', 'Realizar Transferência').should('be.visible')
+    cy.contains('h4','Realizar Transferência').should('be.visible')
   })
 
   it('login com dados invalidos não deve permitir o acesso', () => {
-  
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('111111')
+    cy.fixture('credenciais').then((credenciais) => {
+      cy.get('#username').click().type(credenciais.invalida.usuario)
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
     cy.contains('button','Entrar').click()
     
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
